a65f69dd4bb3c71022f1fe80284ed47db499ff75
nanaknihal <nanaknihal@gmail.com>
2022-10-22 18:47:05 -0400

    re-added files that were needed

---
+++ src/constants/desiredChain.js
@@ -0,0 +1,4 @@
+export const desiredChain = "gnosis";
+export const desiredChainId = 100;
+export const desiredChainRpcUrl = "https://xdai-rpc.gateway.pokt.network";
+export const desiredChainCurrency = "xDAI";
