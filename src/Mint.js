437dbdf05606344b0afda8b0828f850da5ac6bf2
nanaknihal <nanaknihal@gmail.com>
2022-10-22 20:41:16 -0400

    checks whether browser extension is installed

--- src/Mint.js
+++ src/Mint.js
@@ -1,6 +1,27 @@
 import { useState, useEffect } from "react";
 import loadVouched from "./load-vouched";
+import { getExtensionState } from "./utils/extension-helpers";
+
+  // console.log(isInstalled, isRegistered)
+  // if (!isInstalled) {
+  //   document.getElementById("no-extension").style.display="block";
+  // } else if (isInstalled && !isRegistered) {
+  //   document.getElementById("not-registered").style.display="block";
+  // } else if (isInstalled && isRegistered) {
+  //   setupVouched()
+  // } else {
+  //   // TODO: Set error. If this happens, there's probably a bug in the extension
+  // }
+
+
+// Vouched element will not mount unless user 
+    // (a) has extension and 
+    // (b) has set their extension password
+    // waitForInstalled().then(setupVouched)
+    
+
 const Mint = () => {
+    getExtensionState().then(x=>console.log(x))
     useEffect(() => {
         setTimeout(loadVouched,3000);
       }, []);
