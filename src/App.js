6a96db87930e06056f6851d1472df6f2c75d0c57
nanaknihal <nanaknihal@gmail.com>
2022-10-27 16:53:07 -0400

    Brave mentioned as allowed browser

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ import LoadingElement from "./components/loading-element";
 // } from "./components/chain-switcher";
 // import Error from "./components/errors.js";
 import { browserName, isMobile } from "react-device-detect";
-const NotChromeDesktop = () => <><h1>Please make sure you're using Chrome on a desktop</h1><h5>Mobile and other browsers aren't supported in the beta version</h5></>
+const NotChromeDesktop = () => <><h1>Please make sure you're using Chrome or Brave browser, on a desktop computer.</h1><h5>Mobile and other browsers aren't supported in the beta version</h5></>
 
 const Proofs = React.lazy(() => import("./components/proofs"));
 
@@ -47,7 +47,7 @@ function App() {
   }, []);
 
   // let myHoloPage = <AuthenticationFlow />;
-  
+
   if((browserName !== "Chrome") || isMobile) return <NotChromeDesktop />
   return (
     <Suspense fallback={<LoadingElement />}>
