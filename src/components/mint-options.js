caadc32937fa1ad99a8846ccc0c3eb847171c9c6
Nanak Nihal Singh Khalsa <nanaknihal@gmail.com>
2022-12-25 21:47:58 -0800

    new UI

--- src/components/mint-options.js
+++ src/components/mint-options.js
@@ -3,17 +3,50 @@ import { useNavigate } from "react-router-dom";
 import { useAccount, useNetwork } from 'wagmi'
 // import { InfoButton } from "./info-button";
 import RoundedWindow from "./RoundedWindow";
-import { chainIdUsedForLit } from '../constants/misc';
+import { chainIdUsedForLit } from "../constants/misc";
+import phoneImg from "../img/phone.png";
+import idImg from "../img/id.png";
+import moneyImg from "../img/money.png";
 
 const opts = [
-    {name : "Phone Number", url : "/mint/phone", disabled : false},
-    {name : "ID + Phone Number", url : "/mint/idgov", disabled : false},
-    {name : "Accredited Investor Status", url : "/", disabled : true}
+    { 
+        name: "Phone Number", 
+        url: "/mint/phone", 
+        image: phoneImg, 
+        description: "This adds a phone number to your Holo if it's a real number / not a burner. It lets you to prove you're not a bot, for Sybil resistance.", 
+        disabled: false 
+    },
+    { name: 
+        "ID + Phone Number", 
+        url: "/mint/idgov", 
+        image: idImg, 
+        description: "This verifies your government ID. You can prove you're not a bot for Sybil resistance. You can prove facts about your age, jurisdiction, and KYC/AML checks", 
+        disabled: false },
+    { 
+        name: "Accredited Investor Status", 
+        url: "/", image: moneyImg, 
+        description: "This allows you to prove you are an accredited investor. It currently is not implemented", 
+        disabled: true 
+    }
 ];
-const MintOptions = () => {
+
+const MintOption = (props) => {
     const navigate = useNavigate();
+    return <button onClick={()=>navigate(props.url)} className={"x-card blue" + (props.disabled ? " disable" : "")} style={{marginTop: "16px", marginLeft : "100px", marginRight : "100px", fontSize: "x-large"}}>
+                <div style={{display:"flex", flexWrap: "wrap", alignItems: "flex-start", justifyContent: "flex-start", flexDirection: "row"}}>
+                    <img src={props.image} style={{height:"50px", marginRight: "20px"}}></img>
+                    <h5>{props.name}</h5>
+                </div>
+
+                <div style={{display:"flex", flexWrap: "wrap", alignItems: "flex-start", justifyContent: "flex-start", flexDirection: "row", textAlign: "left"}}>
+                    <p>{props.description}</p>
+                </div>
+            </button>
+}
+
+const MintOptions = () => {
     const { data: account } = useAccount();
-    const { switchNetwork } = useNetwork()
+    const { switchNetwork } = useNetwork();
 
     useEffect(() => {
         if (!account) return;
@@ -21,26 +54,20 @@ const MintOptions = () => {
         switchNetwork(chainIdUsedForLit)
     }, [account, switchNetwork])
 
-    return <RoundedWindow>
-            <div className="x-wrapper small-center" style={{width:"100%"}}>
-                <h1>Verify yourself, with privacy</h1>
-                <h5 className="h5">If you don't have a Holo, you can mint one with the following credentials. If you already have one, you can add one of the following credentials to it</h5> 
+    return /*<RoundedWindow>*/<div className="x-wrapper small-center" style={{height: "95%", width:"100%"}}>
+                <h1>Choose your private credentials</h1>
+                <h5 className="h5">to add to your Holo. If you don't have a Holo, this will mint it for you.</h5> 
                     {/* <h4>Warning: these become more private as time passes. For extra privacy, feel free to wait a bit</h4>
                     <InfoButton
                         type="proofMenu"
                         text={`Anonymity is provided by the anonymity set, a.k.a. Privacy Pool. If we wanted to spy and you only waited a minute, we could see you verified at a certain time and that some wallet submitted a proof a minute later. We could then guess you were that wallet. But if you waited a whole week, a lot of people have also will have registered, so we can't tell it's you. Everyone's verification would be pooled together, so we would only know the prover was one person in the whole pool. Whether you wait a second, a day, or year depends on how much you want to stay anonymous to Holonym Foundation. If you trust us not to track you, you can prove now...`}
                     /> */}
                 <div style={{display:"flex", flexWrap: "wrap", alignItems: "stretch", justifyContent: "space-around", flexDirection: "column"}}>
-                        {opts.map(opt=><>
-                            <button onClick={()=>navigate(opt.url)} className={"x-card blue" + (opt.disabled ? " disable" : "")} style={{marginTop: "16px", marginLeft : "100px", marginRight : "100px", fontSize: "x-large"}}>
-                                {opt.name}
-                                </button>
-                        </>
-                        )}
+                        {opts.map(opt=><MintOption {...opt} />)}
                 </div>
                 {/* TODO: add buttons for future credential types such as accredited investor status */}
                 {/* <button disabled onClick={()=>navigate("/")} className="x-button secondary">More proofs coming soon</button> */}
             </div>
-    </RoundedWindow>  
+    /*</RoundedWindow>*/
 }
 export default MintOptions;
