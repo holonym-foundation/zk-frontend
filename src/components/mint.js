7ddda72fda3895ca33af3cc17320087c91b5ca7d
Nanak Nihal Singh Khalsa <nanaknihal@gmail.com>
2022-12-26 13:17:08 -0800

    changed link to docs

--- src/components/mint.js
+++ src/components/mint.js
@@ -93,7 +93,7 @@ return <>
   <WithCheckMark size={3}><h2>Success</h2></WithCheckMark>
     <h5>By minting a Holo, you not only created an identity but also made the Privacy Pool (anonymity set) larger</h5>
     <br />
-    <p><a href="https://holonym.id/whitepaper.pdf" target="_blank" style={{color: "#2fd87a", textDecoration: "underline #2fd87a"}}>Learn about the privacy tech</a></p>
+    <p><a href="https://docs.holonym.id" target="_blank" style={{color: "#2fd87a", textDecoration: "underline #2fd87a"}}>Learn about the privacy tech</a></p>
     <p><a href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(toTweet)}`} target="_blank" style={{color: "#2fd87a", textDecoration: "underline #2fd87a"}}>Bring more privacy to the web: Share your privacy pool contribution</a></p>
     {/* <button className="x-button outline">Learn More</button> */}
     {/* <p>Or <a href="https://holonym.id/whitepaper.pdf" target="_blank" style={{color: "#2fd87a", textDecoration: "underline #2fd87a"}}>learn more</a></p> */}
