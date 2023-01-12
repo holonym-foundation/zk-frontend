6cff4a1d06dd8b832801de6b0f8d10a7987a7424
Amos Elmaliah <amosel@gmail.com>
2023-01-11 19:43:48 -0500

    Moved wagmi into the app where all the other provider are at, + fixes that are already done

--- src/components/profile/PublicInfoCard.js
+++ src/components/profile/PublicInfoCard.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { useNavigate } from "react-router";
+import { useNavigate } from "react-router-dom";
 import { InfoButton } from "../info-button";
 import ColoredHorizontalRule from "../atoms/ColoredHorizontalRule";
 
