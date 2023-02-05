a82501c72ac3d57e585c9bd3d31ddbbcdbee543d
calebtuttle <1calebtuttle@gmail.com>
2023-02-05 11:34:04 -0500

    Use original AccountConnectGate

--- src/gate/AccountConnectGate.js
+++ src/gate/AccountConnectGate.js
@@ -1,8 +1,11 @@
 import React from "react";
-import useAccountConnectGate from "./useAccountConnectGate";
+import { useAccount } from "wagmi";
+// import useAccountConnectGate from "./useAccountConnectGate";
 
 export default function AccountConnectGate({ children, fallback, gate }) {
-	const isGateOpen = useAccountConnectGate(gate);
+	// const isGateOpen = useAccountConnectGate(gate);
+	const { data: account } = useAccount();
+	const isGateOpen = gate({ account });
 	if (isGateOpen) {
 		return <>{children}</>;
 	}
