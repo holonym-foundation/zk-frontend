771265d61ea056f719efc9e53f6085d30b8dedfe
calebtuttle <1calebtuttle@gmail.com>
2023-02-14 18:34:45 -0500

    Update craco config

--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const { when, whenDev, whenProd, whenTest, ESLINT_MODES, POSTCSS_MODES } = require("@craco/craco");
+const { ProvidePlugin }= require("webpack")
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 require('dotenv').config();
 
 module.exports = {
@@ -13,103 +15,62 @@ module.exports = {
     webpack: {
         // configure: { /* Any webpack configuration options: https://webpack.js.org/configuration */ },
         configure: (webpackConfig, { env, paths }) => {
-          // Bundle web worker
-          console.log('process.env.BUILD_WEB_WORKER', process.env.BUILD_WEB_WORKER)
-          if (process.env.BUILD_WEB_WORKER === "true") {
-            webpackConfig.module = { rules: [] };
-            webpackConfig.entry = {
-              worker: path.resolve(paths.appPath, "src/web-workers/load-proofs.js"),
-            };
-            webpackConfig.output = {
-              filename: "[name]-bundle.js",
-              path: path.resolve(paths.appBuild, "dist/worker"),
-            };
-            // webpackConfig.module.rules.push({
-            //   test: /\.worker\.js$/,
-            //   use: {
-            //     loader: "worker-loader",
-            //   },
-            // });
-
-            webpackConfig.module.rules.push({
-              test: /\.mjs$/,
-              include: /node_modules/,
-              type: "javascript/auto"
-            });
-            const wasmExtensionRegExp = /\.wasm$/;
-            webpackConfig.resolve.extensions.push('.wasm');
-            webpackConfig.module.rules.forEach(rule => {
-              (rule.oneOf || []).forEach(oneOf => {
-                if (oneOf.loader && oneOf.loader.indexOf('file-loader') >= 0) {
-                  // make file-loader ignore WASM files
-                  oneOf.exclude.push(wasmExtensionRegExp);
-                }
-              });
-            });
-            // add a dedicated loader for WASM
-            webpackConfig.module.rules.push({
-              test: wasmExtensionRegExp,
-              include: path.resolve(__dirname, 'src'),
-              use: [{ loader: require.resolve('wasm-loader'), options: {} }]
-            });
-            
-            webpackConfig.module.rules.push({
-              test: /\.(js)$/,
-              exclude: /node_modules/,
-              use: [
-                {
-                  loader: "babel-loader",
-                  options: {
-                    presets: ["@babel/preset-env"],
-                    plugins: ["@babel/plugin-transform-modules-commonjs"],
-                  },
-                },
-              ],
-            });
-            
-            return webpackConfig;
+          const wasmExtensionRegExp = /\.wasm$/;
+          webpackConfig.resolve.extensions.push('.wasm');
+          webpackConfig.experiments = {
+            asyncWebAssembly: false,
+            lazyCompilation: true,
+            syncWebAssembly: true,
+            topLevelAwait: true,
+          };
+          webpackConfig.resolve.fallback = {
+            buffer: require.resolve('buffer/')
           }
-          // Bundle main app
-          else {
-            webpackConfig.module.rules.push({
-              test: /\.mjs$/,
-              include: /node_modules/,
-              type: "javascript/auto"
+          webpackConfig.module.rules.forEach((rule) => {
+            (rule.oneOf || []).forEach((oneOf) => {
+              if (oneOf.type === "asset/resource") {
+                oneOf.exclude.push(wasmExtensionRegExp);
+              }
             });
+          });
+          // webpackConfig.plugins.push(new ProvidePlugin({
+          //     Buffer: ['buffer', 'Buffer'],
+          // }));
 
-            const wasmExtensionRegExp = /\.wasm$/;
-
-            webpackConfig.resolve.extensions.push('.wasm');
-
-            webpackConfig.module.rules.forEach(rule => {
-              (rule.oneOf || []).forEach(oneOf => {
-                if (oneOf.loader && oneOf.loader.indexOf('file-loader') >= 0) {
-                  // make file-loader ignore WASM files
-                  oneOf.exclude.push(wasmExtensionRegExp);
-                }
-              });
-            });
-
-            // add a dedicated loader for WASM
-            webpackConfig.module.rules.push({
-              test: wasmExtensionRegExp,
-              include: path.resolve(__dirname, 'src'),
-              use: [{ loader: require.resolve('wasm-loader'), options: {} }]
-            });
-
-            // // Add web assembly support
-            // if (webpackConfig.module.experiments) {
-            //   webpackConfig.module.experiments.push({
-            //     asyncWebAssembly: true,
-            //   });
-            // } else {
-            //   webpackConfig.module.experiments= {
-            //     asyncWebAssembly: true,
-            //   };
-            // }
-
-            return webpackConfig;
+          // Banana wallet config
+          webpackConfig.module.rules = webpackConfig.module.rules.map(rule => {
+            if (rule.oneOf instanceof Array) {
+              rule.oneOf[rule.oneOf.length - 1].exclude = [/\.(js|mjs|jsx|cjs|ts|tsx)$/, /\.wasm$/, /\.html$/, /\.json$/];
+            }
+            return rule;
+          });
+          webpackConfig.resolve.fallback = {
+            ...webpackConfig.resolve.fallback,
+            stream: require.resolve("stream-browserify"),
+            buffer: require.resolve("buffer"),
+            crypto: require.resolve("crypto-browserify"),
+            process: require.resolve("process"),
+            os: require.resolve("os-browserify"),
+            path: require.resolve("path-browserify"),
+            // constants: require.resolve("constants-browserify"),
+            constants: false,
+            fs: false,
           }
+          webpackConfig.resolve.extensions = [...webpackConfig.resolve.extensions, ".ts", ".js"]
+          webpackConfig.plugins = [
+            ...webpackConfig.plugins,
+            new ProvidePlugin({
+              Buffer: ["buffer", "Buffer"],
+            }),
+            new ProvidePlugin({
+              process: ["process"]
+            }),
+            // new NodePolyfillPlugin({
+            //   excludeAliases: ["console"]
+            // }),
+          ]
+
+          return webpackConfig;
         }
     },
     plugins: [
