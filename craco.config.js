ceb9cd2df71657bb3a539e3c709a631178d825ec
calebtuttle <1calebtuttle@gmail.com>
2023-02-13 23:14:07 -0500

    Modify craco config. Successful compilation

--- craco.config.js
+++ craco.config.js
@@ -14,87 +14,124 @@ module.exports = {
     webpack: {
         // configure: { /* Any webpack configuration options: https://webpack.js.org/configuration */ },
         configure: (webpackConfig, { env, paths }) => {
-          webpackConfig.module.rules.push({
-            test: /\.mjs$/,
-            include: /node_modules/,
-            type: "javascript/auto"
-          });
-
           const wasmExtensionRegExp = /\.wasm$/;
-
-          webpackConfig.resolve.extensions.push('.wasm');
-
-          webpackConfig.module.rules.forEach(rule => {
-            (rule.oneOf || []).forEach(oneOf => {
-              (oneOf.use || []).forEach(use => {
-                if (use.loader && use.loader.indexOf('file-loader') >= 0) {
-                  // make file-loader ignore WASM files
-                  if (oneOf.exclude)
-                    oneOf.exclude.push(wasmExtensionRegExp);
-                  else
-                    oneOf.exclude = [wasmExtensionRegExp];
-                }
-              })
-              // if (oneOf.loader && oneOf.loader.indexOf('file-loader') >= 0) {
-              //   // make file-loader ignore WASM files
-              //   oneOf.exclude.push(wasmExtensionRegExp);
-              // }
-            });
-          });
-
-          // // add a dedicated loader for WASM // webpack v4
-          // webpackConfig.module.rules.push({
-          //   test: wasmExtensionRegExp,
-          //   include: path.resolve(__dirname, 'src'),
-          //   use: [{ loader: require.resolve('wasm-loader'), options: {} }]
-          // });
-
-          // Add web assembly support
-          if (webpackConfig.experiments) {
-            webpackConfig.experiments.push({
-              futureDefaults: true,
-              syncWebAssembly: true,
-            });
-          } else {
+            webpackConfig.resolve.extensions.push('.wasm');
             webpackConfig.experiments = {
-              futureDefaults: true,
-              syncWebAssembly: true,
+                asyncWebAssembly: false,
+                lazyCompilation: true,
+                syncWebAssembly: true,
+                topLevelAwait: true,
             };
-          }
+            webpackConfig.resolve.fallback = {
+                buffer: require.resolve('buffer/')
+            }
+            webpackConfig.module.rules.forEach((rule) => {
+                (rule.oneOf || []).forEach((oneOf) => {
+                    if (oneOf.type === "asset/resource") {
+                        oneOf.exclude.push(wasmExtensionRegExp);
+                    }
+                });
+            });
+            webpackConfig.plugins.push(new ProvidePlugin({
+                Buffer: ['buffer', 'Buffer'],
+            }));
 
-          // Banana wallet config
-          // webpackConfig.module.rules = webpackConfig.module.rules.map(rule => {
-          //   if (rule.oneOf instanceof Array) {
-          //     rule.oneOf[rule.oneOf.length - 1].exclude = [/\.(js|mjs|jsx|cjs|ts|tsx)$/, /\.html$/, /\.json$/];
-          //   }
-          //   return rule;
-          // });
           webpackConfig.resolve.fallback = {
-          //   ...webpackConfig.resolve.fallback,
-          //   stream: require.resolve("stream-browserify"),
-          //   buffer: require.resolve("buffer"),
-          //   crypto: require.resolve("crypto-browserify"),
-          //   process: require.resolve("process"),
-          //   os: require.resolve("os-browserify"),
-            // path: require.resolve("path-browserify"),
-          //   constants: require.resolve("constants-browserify"), 
-            // fs: false,
+            ...webpackConfig.resolve.fallback,
+            stream: require.resolve("stream-browserify"),
+            buffer: require.resolve("buffer"),
+            crypto: require.resolve("crypto-browserify"),
+            process: require.resolve("process"),
+            os: require.resolve("os-browserify"),
+            path: require.resolve("path-browserify"),
+            // constants: require.resolve("constants-browserify"),
+            constants: false,
+            fs: false,
           }
-          // webpackConfig.resolve.extensions = [...webpackConfig.resolve.extensions, ".ts", ".js"]
-          webpackConfig.plugins = [
-            ...webpackConfig.plugins,
-            // new ProvidePlugin({
-            //   Buffer: ["buffer", "Buffer"],
-            // }),
-            // new ProvidePlugin({
-            //     process: ["process"]
-            // }),
-            new NodePolyfillPlugin({
-              excludeAliases: ["console"]
-            }),
-          ]
 
-          return webpackConfig;
+            return webpackConfig;
+
+          // webpackConfig.module.rules.push({
+          //   test: /\.mjs$/,
+          //   include: /node_modules/,
+          //   type: "javascript/auto"
+          // });
+
+          // const wasmExtensionRegExp = /\.wasm$/;
+
+          // webpackConfig.resolve.extensions.push('.wasm');
+
+          // webpackConfig.module.rules.forEach(rule => {
+          //   (rule.oneOf || []).forEach(oneOf => {
+          //     (oneOf.use || []).forEach(use => {
+          //       if (use.loader && use.loader.indexOf('file-loader') >= 0) {
+          //         // make file-loader ignore WASM files
+          //         if (oneOf.exclude)
+          //           oneOf.exclude.push(wasmExtensionRegExp);
+          //         else
+          //           oneOf.exclude = [wasmExtensionRegExp];
+          //       }
+          //     })
+          //     // if (oneOf.loader && oneOf.loader.indexOf('file-loader') >= 0) {
+          //     //   // make file-loader ignore WASM files
+          //     //   oneOf.exclude.push(wasmExtensionRegExp);
+          //     // }
+          //   });
+          // });
+
+          // // // add a dedicated loader for WASM // webpack v4
+          // // webpackConfig.module.rules.push({
+          // //   test: wasmExtensionRegExp,
+          // //   include: path.resolve(__dirname, 'src'),
+          // //   use: [{ loader: require.resolve('wasm-loader'), options: {} }]
+          // // });
+
+          // // Add web assembly support
+          // if (webpackConfig.experiments) {
+          //   webpackConfig.experiments.push({
+          //     futureDefaults: true,
+          //     syncWebAssembly: true,
+          //   });
+          // } else {
+          //   webpackConfig.experiments = {
+          //     futureDefaults: true,
+          //     syncWebAssembly: true,
+          //   };
+          // }
+
+          // // Banana wallet config
+          // // webpackConfig.module.rules = webpackConfig.module.rules.map(rule => {
+          // //   if (rule.oneOf instanceof Array) {
+          // //     rule.oneOf[rule.oneOf.length - 1].exclude = [/\.(js|mjs|jsx|cjs|ts|tsx)$/, /\.html$/, /\.json$/];
+          // //   }
+          // //   return rule;
+          // // });
+          // webpackConfig.resolve.fallback = {
+          // //   ...webpackConfig.resolve.fallback,
+          // //   stream: require.resolve("stream-browserify"),
+          // //   buffer: require.resolve("buffer"),
+          // //   crypto: require.resolve("crypto-browserify"),
+          // //   process: require.resolve("process"),
+          // //   os: require.resolve("os-browserify"),
+          //   // path: require.resolve("path-browserify"),
+          // //   constants: require.resolve("constants-browserify"), 
+          //   fs: 'empty',
+          // }
+          // // webpackConfig.resolve.extensions = [...webpackConfig.resolve.extensions, ".ts", ".js"]
+          // webpackConfig.plugins = [
+          //   ...webpackConfig.plugins,
+          //   // new ProvidePlugin({
+          //   //   Buffer: ["buffer", "Buffer"],
+          //   // }),
+          //   // new ProvidePlugin({
+          //   //     process: ["process"]
+          //   // }),
+          //   new NodePolyfillPlugin({
+          //     excludeAliases: ["console"]
+          //   }),
+          // ]
+
+          // return webpackConfig;
         }
     },
     plugins: [
