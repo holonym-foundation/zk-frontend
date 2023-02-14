23802cae1f9ea8c5580b62eca719d3f74e9171fd
calebtuttle <1calebtuttle@gmail.com>
2023-02-13 23:20:00 -0500

    Modify craco config

--- craco.config.js
+++ craco.config.js
@@ -15,27 +15,34 @@ module.exports = {
         // configure: { /* Any webpack configuration options: https://webpack.js.org/configuration */ },
         configure: (webpackConfig, { env, paths }) => {
           const wasmExtensionRegExp = /\.wasm$/;
-            webpackConfig.resolve.extensions.push('.wasm');
-            webpackConfig.experiments = {
-                asyncWebAssembly: false,
-                lazyCompilation: true,
-                syncWebAssembly: true,
-                topLevelAwait: true,
-            };
-            webpackConfig.resolve.fallback = {
-                buffer: require.resolve('buffer/')
-            }
-            webpackConfig.module.rules.forEach((rule) => {
-                (rule.oneOf || []).forEach((oneOf) => {
-                    if (oneOf.type === "asset/resource") {
-                        oneOf.exclude.push(wasmExtensionRegExp);
-                    }
-                });
+          webpackConfig.resolve.extensions.push('.wasm');
+          webpackConfig.experiments = {
+            asyncWebAssembly: false,
+            lazyCompilation: true,
+            syncWebAssembly: true,
+            topLevelAwait: true,
+          };
+          webpackConfig.resolve.fallback = {
+            buffer: require.resolve('buffer/')
+          }
+          webpackConfig.module.rules.forEach((rule) => {
+            (rule.oneOf || []).forEach((oneOf) => {
+              if (oneOf.type === "asset/resource") {
+                oneOf.exclude.push(wasmExtensionRegExp);
+              }
             });
-            webpackConfig.plugins.push(new ProvidePlugin({
-                Buffer: ['buffer', 'Buffer'],
-            }));
+          });
+          // webpackConfig.plugins.push(new ProvidePlugin({
+          //     Buffer: ['buffer', 'Buffer'],
+          // }));
 
+          // Banana wallet config
+          // webpackConfig.module.rules = webpackConfig.module.rules.map(rule => {
+          //   if (rule.oneOf instanceof Array) {
+          //     rule.oneOf[rule.oneOf.length - 1].exclude = [/\.(js|mjs|jsx|cjs|ts|tsx)$/, /\.html$/, /\.json$/];
+          //   }
+          //   return rule;
+          // });
           webpackConfig.resolve.fallback = {
             ...webpackConfig.resolve.fallback,
             stream: require.resolve("stream-browserify"),
@@ -48,8 +55,21 @@ module.exports = {
             constants: false,
             fs: false,
           }
+          webpackConfig.resolve.extensions = [...webpackConfig.resolve.extensions, ".ts", ".js"]
+          webpackConfig.plugins = [
+            ...webpackConfig.plugins,
+            new ProvidePlugin({
+              Buffer: ["buffer", "Buffer"],
+            }),
+            new ProvidePlugin({
+              process: ["process"]
+            }),
+            // new NodePolyfillPlugin({
+            //   excludeAliases: ["console"]
+            // }),
+          ]
 
-            return webpackConfig;
+          return webpackConfig;
 
           // webpackConfig.module.rules.push({
           //   test: /\.mjs$/,
