d65035e498f30cc6dc3684a5f572eb83187b49ac
calebtuttle <1calebtuttle@gmail.com>
2023-02-14 09:18:09 -0500

    Update craco config

--- craco.config.js
+++ craco.config.js
@@ -37,12 +37,12 @@ module.exports = {
           // }));
 
           // Banana wallet config
-          // webpackConfig.module.rules = webpackConfig.module.rules.map(rule => {
-          //   if (rule.oneOf instanceof Array) {
-          //     rule.oneOf[rule.oneOf.length - 1].exclude = [/\.(js|mjs|jsx|cjs|ts|tsx)$/, /\.html$/, /\.json$/];
-          //   }
-          //   return rule;
-          // });
+          webpackConfig.module.rules = webpackConfig.module.rules.map(rule => {
+            if (rule.oneOf instanceof Array) {
+              rule.oneOf[rule.oneOf.length - 1].exclude = [/\.(js|mjs|jsx|cjs|ts|tsx)$/, /\.wasm$/, /\.html$/, /\.json$/];
+            }
+            return rule;
+          });
           webpackConfig.resolve.fallback = {
             ...webpackConfig.resolve.fallback,
             stream: require.resolve("stream-browserify"),
