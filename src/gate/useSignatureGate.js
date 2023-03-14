e23df0ec48fac95f5cafb9b8d554c034d2efde5f
calebtuttle <1calebtuttle@gmail.com>
2023-03-14 17:06:40 -0400

    Update signature gate to handle banana wallet

--- src/gate/useSignatureGate.js
+++ src/gate/useSignatureGate.js
@@ -1,19 +1,36 @@
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 import { ethers } from "ethers";
-// import { useAccount } from "wagmi";
+import { useAccount as useWagmiAccount } from "wagmi";
 import useAccount from "../hooks/useAccount";
 import { useHoloAuthSig } from "../context/HoloAuthSig";
 import { useHoloKeyGenSig } from "../context/HoloKeyGenSig";
 import { holonymAuthMessage, holonymKeyGenMessage } from "../constants";
 
 export default function useSignatureGate(gate) {
+	const { data: wagmiAccount } = useWagmiAccount();
 	const { data: account } = useAccount();
 	const {
-		signHoloAuthMessage, holoAuthSigIsError, holoAuthSigIsLoading, holoAuthSigIsSuccess, holoAuthSig, holoAuthSigDigest, clearHoloAuthSig,
+		signHoloAuthMessage,
+		holoAuthSigIsError,
+		holoAuthSigIsLoading,
+		holoAuthSigIsSuccess,
+		holoAuthSig,
+		holoAuthSigDigest,
+		clearHoloAuthSig,
 	} = useHoloAuthSig();
 	const {
-		signHoloKeyGenMessage, holoKeyGenSigIsError, holoKeyGenSigIsLoading, holoKeyGenSigIsSuccess, holoKeyGenSig, holoKeyGenSigDigest, clearHoloKeyGenSig,
+		signHoloKeyGenMessage,
+		holoKeyGenSigIsError,
+		holoKeyGenSigIsLoading,
+		holoKeyGenSigIsSuccess,
+		holoKeyGenSig,
+		holoKeyGenSigDigest,
+		clearHoloKeyGenSig,
 	} = useHoloKeyGenSig();
+	const usingWagmi = useMemo(
+		() => !!wagmiAccount?.address && !!wagmiAccount?.connector,
+		[wagmiAccount]
+	);
 
 	useEffect(
 		() => {
@@ -47,18 +64,24 @@ export default function useSignatureGate(gate) {
 		if (!account?.address || !account?.connector)
 			return;
 		// Check that sigs are from account. If they aren't, re-request them
-		if (holoAuthSig &&
-			ethers.utils.verifyMessage(holonymAuthMessage, holoAuthSig) !==
-			account.address) {
-			console.log("account changed. Re-retrieving holoAuthSig");
-			clearHoloAuthSig();
-			signHoloAuthMessage().catch((err) => console.error(err));
+		if (usingWagmi) {
+			if (holoAuthSig && ethers.utils.verifyMessage(holonymAuthMessage, holoAuthSig) !== account.address) {
+				console.log("account changed. Re-retrieving holoAuthSig");
+				clearHoloAuthSig();
+				signHoloAuthMessage().catch((err) => console.error(err));
+			}
+			if (holoKeyGenSig && ethers.utils.verifyMessage(holonymKeyGenMessage, holoKeyGenSig) !== account.address) {
+				console.log("account changed. Re-retrieving holoKeyGenSig");
+				clearHoloKeyGenSig();
+				signHoloKeyGenMessage().catch((err) => console.error(err));
+			}
 		}
-		if (holoKeyGenSig &&
-			ethers.utils.verifyMessage(holonymKeyGenMessage, holoKeyGenSig) !==
-			account.address) {
-			console.log("account changed. Re-retrieving holoKeyGenSig");
+		// If user is using Banana wallet, we assume the signatures need to be re-requested
+		else {
+			// TODO: Verify the signatures. Figure out which scheme Banana wallet uses
+			clearHoloAuthSig();
 			clearHoloKeyGenSig();
+			signHoloAuthMessage().catch((err) => console.error(err));
 			signHoloKeyGenMessage().catch((err) => console.error(err));
 		}
 	}, [account]);
