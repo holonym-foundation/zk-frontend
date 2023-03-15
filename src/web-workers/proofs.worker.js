cae4ee9e2f13d7d5dcc6f9adc2e6c8330c37aad7
calebtuttle <1calebtuttle@gmail.com>
2023-03-14 21:09:14 -0400

    Refactor proofs.worker.js

--- src/web-workers/proofs.worker.js
+++ src/web-workers/proofs.worker.js
@@ -25,99 +25,46 @@ let generatingProof = {
 	'kolp': false, // == "Knowlege of Leaf Preimage"
 }
 
-// TODO: Low priority: Refactor this to have less duplicated code.
+async function loadProof(proofFunction, args, proofType, forceReload) {
+	if (generatingProof[proofType] && !forceReload) return;
+	try {
+		generatingProof[proofType] = true;
+		console.log(`[Worker] Generating ${proofType} proof. Received params:`, args)
+		const antiSybilProof = await proofFunction(...args);
+		postMessage({ error: null, proofType, proof: antiSybilProof });
+	} catch (err) {
+		console.log(`[Worker] Error generating ${proofType} proof`, err)
+		postMessage({ error: err, proofType, proof: null });
+	} finally {
+		generatingProof[proofType] = false;
+	}
+}
+
 onmessage = async (event) => {
-	await waitForArtifacts('poseidonQuinary', 10 * 1000)
-	await waitForArtifacts('poseidonTwoInputs', 10 * 1000)
+	await waitForArtifacts('poseidonQuinary', 10 * 1000);
+	await waitForArtifacts('poseidonTwoInputs', 10 * 1000);
   if (event.data && event.data.message === "uniqueness") {
-		try {
-			if (generatingProof['uniqueness'] && !event.data.forceReload) return;
-			generatingProof['uniqueness'] = true;
-			console.log('[Worker] Generating uniqueness proof. Received params:', event.data)
-			const antiSybilProof = await antiSybil(
-				event.data.userAddress,
-				event.data.govIdCreds,
-				event.data.actionId,
-			);
-			postMessage({ error: null, proofType: "uniqueness", proof: antiSybilProof });
-		} catch (err) {
-			console.log('[Worker] Error generating uniqueness proof', err)
-			generatingProof['uniqueness'] = false;
-			postMessage({ error: err, proofType: "uniqueness", proof: null });
-		}
+		const args = [
+			event.data.userAddress,
+			event.data.govIdCreds,
+			event.data.actionId,
+		];
+		loadProof(antiSybil, args, 'uniqueness', event.data.forceReload);
   } else if (event.data && event.data.message === "uniqueness-phone") {
-		try {
-			if (generatingProof['uniqueness-phone'] && !event.data.forceReload) return;
-			generatingProof['uniqueness-phone'] = true;
-			console.log('[Worker] Generating uniqueness-phone proof. Received params:', event.data)
-			const uniquenessPhoneProof = await uniquenessPhone(
-				event.data.userAddress,
-				event.data.govIdCreds,
-				event.data.actionId,
-			);
-			postMessage({ error: null, proofType: "uniqueness-phone", proof: uniquenessPhoneProof });
-		} catch (err) {
-			console.log('[Worker] Error generating uniqueness-phone proof', err)
-			generatingProof['uniqueness-phone'] = false;
-			postMessage({ error: err, proofType: "uniqueness-phone", proof: null });
-		}
+		const args = [event.data.userAddress, event.data.phoneNumCreds, event.data.actionId];
+		loadProof(uniquenessPhone, args, 'uniqueness-phone', event.data.forceReload);
   } else if (event.data && event.data.message === "us-residency") {
-		try {
-			if (generatingProof['us-residency'] && !event.data.forceReload) return;	
-			generatingProof['us-residency'] = true;
-			console.log('[Worker] Generating us-residency proof. Params:', event.data)
-			const proofOfResidencyProof = await proofOfResidency(event.data.userAddress, event.data.govIdCreds);
-			postMessage({ error: null, proofType: "us-residency", proof: proofOfResidencyProof });
-		} catch (err) {
-			console.log('[Worker] Error generating us-residency proof', err)
-			generatingProof['us-residency'] = false;
-			postMessage({ error: err, proofType: "us-residency", proof: null })
-		}
+		const args = [event.data.userAddress, event.data.govIdCreds];
+		loadProof(proofOfResidency, args, 'us-residency', event.data.forceReload);
   } else if (event.data && event.data.message === "medical-specialty") {
-		try {
-			if (generatingProof['medical-specialty'] && !event.data.forceReload) return;	
-			generatingProof['medical-specialty'] = true;
-			console.log('[Worker] Generating medical-specialty proof. Params:', event.data)
-			const medicalSpecialtyProof = await proofOfMedicalSpecialty(
-				event.data.userAddress,
-				event.data.medicalCreds,
-			);
-			generatingProof['medical-specialty'] = false;
-			postMessage({ error: null, proofType: "medical-specialty", proof: medicalSpecialtyProof });
-		} catch (err) {
-			console.log('[Worker] Error generating medical-specialty proof', err)
-			generatingProof['medical-specialty'] = false;
-			postMessage({ error: err, proofType: "medical-specialty", proof: null })
-		}
+		const args = [event.data.userAddress, event.data.medicalCreds];
+		loadProof(proofOfMedicalSpecialty, args, 'medical-specialty', event.data.forceReload);
   } else if (event.data && event.data.message === "gov-id-firstname-lastname") {
-		try {
-			if (generatingProof['gov-id-firstname-lastname'] && !event.data.forceReload) return;	
-			generatingProof['gov-id-firstname-lastname'] = true;
-			console.log('[Worker] Generating gov-id-firstname-lastname proof. Params:', event.data)
-      const govIdFirstNameLastNameProof = await proveGovIdFirstNameLastName(event.data.govIdCreds);
-			generatingProof['gov-id-firstname-lastname'] = false;
-			postMessage({ error: null, proofType: "gov-id-firstname-lastname", proof: govIdFirstNameLastNameProof });
-		} catch (err) {
-			console.log('[Worker] Error generating gov-id-firstname-lastname proof', err)
-			generatingProof['gov-id-firstname-lastname'] = false;
-			postMessage({ error: err, proofType: "gov-id-firstname-lastname", proof: null })
-		}
+		const args = [event.data.govIdCreds];
+		loadProof(proveGovIdFirstNameLastName, args, 'gov-id-firstname-lastname', event.data.forceReload);
   } else if (event.data && event.data.message === "kolp") {
-		try {
-			if (generatingProof['kolp'] && !event.data.forceReload) return;	
-			generatingProof['kolp'] = true;
-			console.log('[Worker] Generating kolp proof. Params:', event.data)
-			const kolpProof = await proveKnowledgeOfLeafPreimage(
-				event.data.serializedAsNewPreimage, 
-				event.data.newSecret
-			);
-			generatingProof['kolp'] = false;
-			postMessage({ error: null, proofType: "kolp", proof: kolpProof});
-		} catch (err) {
-			console.log('[Worker] Error generating kolp proof', err)
-			generatingProof['kolp'] = false;
-			postMessage({ error: err, proofType: "kolp", proof: null })
-		}
+		const args = [event.data.serializedAsNewPreimage, event.data.newSecret];
+		loadProof(proveKnowledgeOfLeafPreimage, args, 'kolp', event.data.forceReload);
   } else {
     postMessage({ error: "Unknown message", proofType: null, proof: null });
   }
