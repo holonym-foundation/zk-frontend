83fdaeabf2b921ea5c9815ab463c9a680c876961
calebtuttle <1calebtuttle@gmail.com>
2023-07-18 15:30:18 -0400

    Add "add optimism" functionality

--- src/components/atoms/SwitchChains.js
+++ src/components/atoms/SwitchChains.js
@@ -3,10 +3,32 @@ import { desiredChainId } from '../../constants'
 
 const SwitchChains = () => {
   const {
+    error,
     switchNetwork,
   } = useNetwork({
     chainId: desiredChainId
   });
+
+  async function addOptimism() {
+    try {
+      await window.ethereum.request({
+        method: 'wallet_addEthereumChain',
+        params: [{
+          chainId: '0xa',
+          chainName: 'Optimism',
+          nativeCurrency: {
+            name: 'Ether',
+            symbol: 'ETH',
+            decimals: 18
+          },
+          rpcUrls: ['https://mainnet.optimism.io'],
+          blockExplorerUrls: ['https://optimistic.etherscan.io']
+        }]
+      })
+    } catch (err) {
+      console.error(err)
+    }
+  }
 
   return (
     <>
@@ -20,13 +42,28 @@ const SwitchChains = () => {
           <div style={{ width: "100", fontSize: "18px", marginTop: "20px", marginBottom: "30px", maxWidth: "400px" }}>
             <div className="nav-wallet" style={{ backgroundColor: 'var(--dark-card-background)' }}>
               <div className="nav-wallet-text nav-link w-nav-link" onClick={() => {
-                console.log('SwitchChains component: switchNetwork', switchNetwork)
                 switchNetwork()
               }}>
                 Switch Chains
               </div>
-            </div>       
+            </div>      
           </div>
+
+          {error && (
+            <>
+              <p style={{ color: 'red' }}>
+                An error occurred trying to switch chains. Please make sure you have Optimism
+                added to your wallet.
+              </p>
+              <div style={{ width: "100", fontSize: "18px", marginTop: "20px", marginBottom: "30px", maxWidth: "400px" }}>
+                <div className="nav-wallet" style={{ backgroundColor: 'var(--dark-card-background)' }}>
+                  <div className="nav-wallet-text nav-link w-nav-link" onClick={() => addOptimism()}>
+                    Add Optimism
+                  </div>
+                </div>      
+              </div>
+            </>
+          )}
         </div>
       </div>
     </>
