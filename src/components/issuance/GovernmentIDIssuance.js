19992a64e830cb4fa84f69d48e8b0623a355300b
calebtuttle <1calebtuttle@gmail.com>
2023-08-02 15:50:16 -0400

    use idenfy

--- src/components/issuance/GovernmentIDIssuance.js
+++ src/components/issuance/GovernmentIDIssuance.js
@@ -3,11 +3,88 @@ import { useNavigate, useParams } from "react-router-dom";
 import { createVeriffFrame, MESSAGES } from '@veriff/incontext-sdk';
 import { useQuery } from '@tanstack/react-query'
 import loadVouched from '../../load-vouched';
+import { useHoloAuthSig } from "../../context/HoloAuthSig";
 import FinalStep from "./FinalStep";
 import StepSuccess from "./StepSuccess";
 import { idServerUrl, maxDailyVouchedJobCount } from "../../constants";
 import VerificationContainer from "./IssuanceContainer";
 import { datadogLogs } from "@datadog/browser-logs";
+
+const useVeriffIDV = ({ enabled }) => {
+  const navigate = useNavigate();
+  const veriffSessionQuery = useQuery({
+    queryKey: ['veriffSession'],
+    queryFn: async () => {
+      const resp = await fetch(`${idServerUrl}/veriff/session`, {
+        method: "POST",
+      })
+      return await resp.json()
+    },
+    staleTime: Infinity,
+    enabled: enabled
+  });
+
+  useEffect(() => {
+    if (!veriffSessionQuery.data?.url || !enabled) return;
+    const verification = veriffSessionQuery.data;
+    const handleVeriffEvent = (msg) => {
+      if (msg === MESSAGES.FINISHED) {
+        const retrievalEndpoint = `${idServerUrl}/veriff/credentials?sessionId=${verification.id}`
+        const encodedRetrievalEndpoint = encodeURIComponent(window.btoa(retrievalEndpoint))
+        navigate(`/issuance/idgov/store?retrievalEndpoint=${encodedRetrievalEndpoint}`)
+      }
+    }
+    createVeriffFrame({
+      url: verification.url,
+      onEvent: handleVeriffEvent
+    });
+  }, [veriffSessionQuery])
+
+  return {}
+}
+
+const useIdenfyIDV = ({ enabled }) => {
+  const navigate = useNavigate();
+  const { holoAuthSigDigest } = useHoloAuthSig();
+  const [retrievalEndpoint, setRetrievalEndpoint] = useState('');
+
+  const idenfySessionQuery = useQuery({
+    queryKey: ['idenfySession'],
+    queryFn: async () => {
+      const resp = await fetch(`${idServerUrl}/idenfy/session`, {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          sigDigest: holoAuthSigDigest
+        })
+      })
+      return await resp.json()
+    },
+    staleTime: Infinity,
+    enabled: holoAuthSigDigest && enabled
+  });
+
+  useEffect(() => {
+    if (!idenfySessionQuery.data?.url) return;
+    const verification = idenfySessionQuery.data;
+    // Open verification.url in new tab
+    // window.open(verification.url, '_blank');
+
+    // When user clicks button, navigate to retrievalEndpoint
+    // TODO: Instead of waiting for user to click button, poll id-server for verification status
+    const retrievalEndpoint = `${idServerUrl}/idenfy/credentials?scanRef=${verification.scanRef}`
+    const encodedRetrievalEndpoint = encodeURIComponent(window.btoa(retrievalEndpoint))
+    // navigate(`/issuance/idgov/store?retrievalEndpoint=${encodedRetrievalEndpoint}`)
+    setRetrievalEndpoint(encodedRetrievalEndpoint);
+  }, [idenfySessionQuery])
+
+  return {
+    verificationUrl: idenfySessionQuery.data?.url,
+    retrievalEndpoint
+  }
+}
 
 const StepSuccessWithAnalytics = () => {
   useEffect(() => {
@@ -30,33 +107,17 @@ const StepIDV = () => {
       console.log(err);
     }
   }, []);
-  const navigate = useNavigate();
-  const veriffSessionQuery = useQuery({
-    queryKey: ['veriffSession'],
-    queryFn: async () => {
-      const resp = await fetch(`${idServerUrl}/veriff/session`, {
-        method: "POST",
-      })
-      return await resp.json()
-    },
-    staleTime: Infinity
-  });
 
-  useEffect(() => {
-    if (!veriffSessionQuery.data?.url) return;
-    const verification = veriffSessionQuery.data;
-    const handleVeriffEvent = (msg) => {
-      if (msg === MESSAGES.FINISHED) {
-        const retrievalEndpoint = `${idServerUrl}/veriff/credentials?sessionId=${verification.id}`
-        const encodedRetrievalEndpoint = encodeURIComponent(window.btoa(retrievalEndpoint))
-        navigate(`/issuance/idgov/store?retrievalEndpoint=${encodedRetrievalEndpoint}`)
-      }
-    }
-    createVeriffFrame({
-      url: verification.url,
-      onEvent: handleVeriffEvent
-    });
-  }, [veriffSessionQuery])
+  const navigate = useNavigate();
+  const preferredProvider = 'idenfy' // TODO: Make this a function of IP address location
+  useVeriffIDV({
+    // TODO: Make enabled dependent on IP address location
+    enabled: false
+  })
+  const { verificationUrl, retrievalEndpoint } = useIdenfyIDV({
+    // TODO: Make enabled dependent on IP address location
+    enabled: true
+  })
 
   // useEffect(() => {
   //   if (!phoneNumber) return;
@@ -79,7 +140,53 @@ const StepIDV = () => {
   return (
     <>
       <h3 style={{marginBottom:"25px", marginTop: "-25px"}}>Verify your ID</h3>
-      <div id="vouched-element" style={{ height: "10vh" }} />
+      {/* <div id="vouched-element" style={{ height: "10vh" }} /> */}
+
+      {preferredProvider === 'idenfy' && (
+        <div style={{ textAlign: 'center' }}>
+          <p>Verify your ID</p>
+          <div style={{ 
+            marginTop: '20px',
+            marginBottom: '20px',
+            display: 'flex',
+            justifyContent: 'center'
+          }}>
+            <button
+              className="export-private-info-button"
+              style={{
+                lineHeight: "1",
+                fontSize: "16px"
+              }}
+              disabled={!retrievalEndpoint}
+              onClick={() => {
+                window.open(verificationUrl, '_blank');
+              }}
+            >
+              Start Verification
+            </button>
+          </div>
+          <p>Click continue once you are verified</p>
+          <div style={{ 
+            marginTop: '20px',
+            display: 'flex',
+            justifyContent: 'center'
+          }}>
+            <button
+              className="export-private-info-button"
+              style={{
+                lineHeight: "1",
+                fontSize: "16px"
+              }}
+              disabled={!retrievalEndpoint}
+              onClick={() => {
+                navigate(`/issuance/idgov/store?retrievalEndpoint=${retrievalEndpoint}`)
+              }}
+            >
+              Continue
+            </button>
+          </div>
+        </div>
+      )}
     </>
   );
 }
