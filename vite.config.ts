2c918cb3262a2aafb80e424125a1ed25d37fc4c9
calebtuttle <1calebtuttle@gmail.com>
2023-08-19 20:49:17 -0400

    Add vite-plugin-node-polyfills

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import wasm from "vite-plugin-wasm"
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import topLevelAwait from "vite-plugin-top-level-await";
 
 // https://vitejs.dev/config/
@@ -10,6 +11,20 @@ export default defineConfig({
     port: 3002,
   },
   plugins: [
+    nodePolyfills({
+      // To exclude specific polyfills, add them to this list.
+      // exclude: [
+      //   'fs', // Excludes the polyfill for `fs` and `node:fs`.
+      // ],
+      // // Whether to polyfill specific globals.
+      // globals: {
+      //   Buffer: true, // can also be 'build', 'dev', or false
+      //   global: true,
+      //   process: true,
+      // },
+      // Whether to polyfill `node:` protocol imports.
+      protocolImports: true,
+    }),
     NodeGlobalsPolyfillPlugin({
       buffer: true,
       process: true,
@@ -21,6 +36,20 @@ export default defineConfig({
   worker: {
     format: 'es',
     plugins: [
+      nodePolyfills({
+        // To exclude specific polyfills, add them to this list.
+        // exclude: [
+        //   'fs', // Excludes the polyfill for `fs` and `node:fs`.
+        // ],
+        // // Whether to polyfill specific globals.
+        // globals: {
+        //   Buffer: true, // can also be 'build', 'dev', or false
+        //   global: true,
+        //   process: true,
+        // },
+        // Whether to polyfill `node:` protocol imports.
+        protocolImports: true,
+      }),
       NodeGlobalsPolyfillPlugin({
         buffer: true,
         process: true,
