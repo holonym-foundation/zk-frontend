58d17f549080e31b0f3679e027159461903fb7d3
calebtuttle <1calebtuttle@gmail.com>
2023-09-01 20:27:25 -0400

    Add useCreateIdServerSession hook

---
+++ src/hooks/useCreateIdServerSession.ts
@@ -0,0 +1,31 @@
+import { useMutation } from "@tanstack/react-query";
+import { idServerUrl } from "../constants";
+import { useHoloAuthSig } from "../context/HoloAuthSig";
+
+function useCreateIdServerSession({ preferredProvider }: { preferredProvider?: string }) {
+  const { holoAuthSigDigest } = useHoloAuthSig();
+
+  return useMutation(
+    async () => {
+      if (!preferredProvider) throw new Error("No preferred provider");
+      const resp = await fetch(`${idServerUrl}/sessions`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          sigDigest: holoAuthSigDigest,
+          idvProvider: preferredProvider,
+        }),
+      })
+  
+      if (!resp.ok) {
+        throw new Error("Failed to create session")
+      }
+  
+      return resp.json()
+    }
+  )
+}
+
+export default useCreateIdServerSession
