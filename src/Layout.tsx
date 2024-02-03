3b0fc3316c3ece26b144a4d2528bcc6b1aa84533
calebtuttle <1calebtuttle@gmail.com>
2024-02-02 19:44:33 -0500

    announcement frequency

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -23,7 +23,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       // If hash(ip) mod 10 === 0, show the announcement
       const digest = "0x" + (await sha1String(ipAndCountry.ip));
       const result = ethers.BigNumber.from(digest).mod(10).toNumber();
-      if ([0, 1, 2].includes(result)) {
+      if ([0, 1, 2, 3].includes(result)) {
         setShowNewHolonymAnnouncement(true);
       } else {
         // (For devs) If localStorage has the key "showNewHolonymAnnouncement", show the announcement
